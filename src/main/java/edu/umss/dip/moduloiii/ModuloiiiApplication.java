eaeee906a3ce6ac18531977da370cde79527c192
PC1@Pam <pam16card@gmail.com>
2019-03-06 02:10:17 -0700

    Initial setup

---
+++ src/main/java/edu/umss/dip/moduloiii/ModuloiiiApplication.java
@@ -0,0 +1,13 @@
+package edu.umss.dip.moduloiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloiiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuloiiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/umss/dip/moduloiii/ModuloiiiApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.umss.dip.moduloiii;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModuloiiiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
